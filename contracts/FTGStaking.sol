843d6b687faacbc1038f2715ae28f90219e9d1c8
benjyz <benjyc@proton.me>
2022-10-19 18:48:44 +0700

    imports

--- contracts/FTGStaking.sol
+++ contracts/FTGStaking.sol
@@ -1,38 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
+import "OpenZeppelin/openzeppelin-contracts@4.1.0/contracts/token/ERC20/IERC20.sol";
+import "OpenZeppelin/openzeppelin-contracts@4.1.0/contracts/access/Ownable.sol";
 import "paulrberg/prb-math@2.5.0/contracts/PRBMath.sol";
-import "./Ownable.sol";
 
-interface IERC20 {
-    function totalSupply() external view returns (uint256);
-
-    function balanceOf(address account) external view returns (uint256);
-
-    function transfer(address recipient, uint256 amount)
-        external
-        returns (bool);
-
-    function allowance(address owner, address spender)
-        external
-        view
-        returns (uint256);
-
-    function approve(address spender, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address sender,
-        address recipient,
-        uint256 amount
-    ) external returns (bool);
-
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Approval(
-        address indexed owner,
-        address indexed spender,
-        uint256 value
-    );
-}
 
 contract FTGStaking is Ownable {
     IERC20 public immutable ftgToken;
