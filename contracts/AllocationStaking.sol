46c41a678b14c4b3408da3701be7516b83681791
benjyz <benjyc@proton.me>
2022-10-25 16:26:07 +0700

    base

---
+++ contracts/AllocationStaking.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+// is Ownable
+contract AllocationStaking  {
+
+    // FTGstaking
+
+    // Address of sales factory contract
+    // ISalesFactory public salesFactory;
+
+    // Info of each user.
+    struct UserInfo {
+        //
+        //uint256 tokensUnlockTime; // If user registered for sale, returns when tokens are getting unlocked
+        //address [] salesRegistered;
+    }
+
+    constructor(){
+
+    }
+
+}
