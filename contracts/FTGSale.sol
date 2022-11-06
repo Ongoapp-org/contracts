4199c6922f1c512e97a44e568642b5fbe2c8e6d7
benjyz <benjyc@proton.me>
2022-11-06 16:51:18 +0700

    sale contract

--- contracts/FTGSale.sol
+++ contracts/FTGSale.sol
@@ -11,23 +11,41 @@ import "./FTGStaking.sol";
 //Guaranteed Pool
 //Public Pool
 contract FTGSale is Ownable {
-
     struct Participant {
         address partaddr;
-        uint256 ticketSize;
-        uint256 invested;
+        uint256 amountAllocated;
     }
 
-    string public saleName;
+    // TODO move this is duplicate
+    // New staking or unstaking
+    struct Staking {
+        uint256 totalStaked; // totalStaked after this staking
+        uint256 timestamp; // time of staking
+        int256 amount; // amount of staking (>0 staking, <0 unstaking)
+        uint256 lockDuration; // duration of locked time in secs (flex = 0, LOCK30DAYS = 2592000, LOCK60DAYS = 5184000, LOCK90DAYS = 7776000)
+    }
+
+    enum Tiers {
+        DIAMOND,
+        EMERALD,
+        SAPPHIRE,
+        RUBY
+    }
+
+    string nameSale;
 
     mapping(address => Participant) public participants;
 
-    mapping(address => bool) public whitelist; 
+    mapping(address => bool) public whitelist;
+    //all ticket alocated for each tier
+    mapping(Tiers => uint32) allAllocated;
+    //used ticket for each tocket
+    mapping(Tiers => uint32) ticketAllocated;
 
     // Token being sold
-    address saleToken;
+    IERC20 saleToken;
     // invest token
-    address investToken;
+    IERC20 investToken;
     // Is sale created
     bool isCreated;
     // Are earnings withdrawn
@@ -57,47 +75,63 @@ contract FTGSale is Ownable {
     uint256 sapphireMinimum = 250_000;
     uint256 rubyMinimum = 100_000;
 
-    uint256 public amountGuaranteedPool;
-    uint256 public amountPublicPool;
+    uint32 eachDiamondTicket;
+    uint32 eachEmeraldTicket;
+    uint32 eachSapphireTicket;
+    uint32 eachRubyTicket;
 
-    constructor(string memory _name, address _saleToken, address _investToken, address _stakingContractAddress, uint256 _amountGuaranteedPool, uint256 _amountPublicPool, uint256 _tokenPriceInUSD) {
-        saleToken = _saleToken;
-        investToken = _investToken;
-        saleName = _name;
+    //TODO make a setting
+    uint32 diamondParticipants = 100;
+    uint32 emeraldParticipants = 500;
+    uint32 sapphireParticipants = 1000;
+    uint32 rubyParticipants = 2000;
+
+    //TODO make a setting
+    uint256 amountGuaranteedPool = 1_000_000;
+    uint256 amountPublicPool = 500_000;
+    uint8 factor = 10_000;
+
+    //TODO make a setting
+    uint32 diamondAllocTotal = 40;
+    uint32 emeraldAllocTotal = 30;
+    uint32 sapphireAllocTotal = 20;
+    uint32 rubyAllocTotal = 10;
+
+    constructor(
+        string memory _name,
+        address _stakingContractAddress,
+        uint256 _amountGuaranteedPool,
+        uint256 _tokenPriceInUSD
+    ) {
+        //TODO in constructor
+        
+        allAllocated[Tiers.DIAMOND] = 40 * factor;
+        allAllocated[Tiers.EMERALD] = 30 * factor;
+        allAllocated[Tiers.SAPPHIRE] = 20 * factor;
+        allAllocated[Tiers.RUBY] = 10 * factor;
+
+        eachDiamondTicket = allAllocated[Tiers.DIAMOND] / diamondParticipants;
+        eachEmeraldTicket = allAllocated[Tiers.EMERALD] / emeraldParticipants;
+        eachSapphireTicket =
+            allAllocated[Tiers.SAPPHIRE] /
+            sapphireParticipants;
+        eachRubyTicket = allAllocated[Tiers.RUBY] / rubyParticipants;
+
+        nameSale = _name;
         stakingContract = FTGStaking(_stakingContractAddress);
         amountGuaranteedPool = _amountGuaranteedPool;
         tokenPriceInUSD = _tokenPriceInUSD;
-        amountPublicPool = _amountPublicPool;
     }
 
-    //determine the ticket sizes in %
-    //ticket size is between 0.1% and 2%
-    //TODO 
-    function calculateTicketSize() private returns (uint256) {
-        uint256 ticketAmount = 0;
-        //TODO maybe need to loop through 
-        //calculate lock at least 30days
-        //uint256 amountStaked = stakingContract.stakeholders[msg.sender].stakings;
-        uint256 amountStaked = stakingContract.totalFTGStaked();
-        if (amountStaked > diamondMinimum){
-            //TODO calcualte total available in guarnateed pool and subtract??
-            uint256 participantAmount = 100;
-            participants[msg.sender] = Participant(msg.sender, participantAmount);
-
-            
-        } else if (amountStaked > emeraldMinimum){
-
-        }
-
-        return ticketAmount;
+    // TODO calculate amount eligible
+    function amountEligible(address account) private returns (uint256) {
+        return 0;
     }
 
-    function _checkStaking() private  {
+    function _checkStaking() private {
         //calculate amount staked in 30 days or more
-
         //subtract amount from available pool
         //amountGuaranteedPool -= participantAmount;
-        
     }
 
     function addWhitelist(address p) external onlyOwner {
@@ -106,9 +140,7 @@ contract FTGSale is Ownable {
         //TODO other steps?
     }
 
-
     //checkParticipationSignature
-
 
     //take part in the sale i.e buy tokens, pass signature on frontend
     function participate(uint256 amountTokensBuy) external {
@@ -116,8 +148,8 @@ contract FTGSale is Ownable {
 
         require(whitelist[msg.sender], "not in whitelist");
         //determine allocation size
-        uint256 ticket = calculateTicketSize(msg.sender);
-        require(amountTokensBuy <= ticket, "amount too high not eliglbe");
+        uint256 amountElig = amountEligible(msg.sender);
+        require(amountTokensBuy <= amountElig, "amount too high not eliglbe");
 
         // bytes calldata signature
         // signature verifies KYC
@@ -128,9 +160,6 @@ contract FTGSale is Ownable {
         IERC20(investToken).transferFrom(msg.sender, address(this), costInUSD);
 
         IERC20(saleToken).transfer(msg.sender, amountTokensBuy);
-        //set invested
-
-
     }
 
     // Function for owner to deposit tokens
@@ -142,4 +171,122 @@ contract FTGSale is Ownable {
     //TODO
     //function withdrawRaisedAssets() onlyOwner {}
 
+    // mapping(Tiers => uint128) participants = [];
+
+    // function manipulateAllAllocated(Tiers tier) public {
+    //     if (tier == Tiers.DIAMOND) {} else if (
+    //         tier == Tiers.EMERALD
+    //     ) {} else if (tier == Tiers.SAPPHIRE) {} else if (tier == Tiers.RUBY) {}
+    // }
+
+    // function getStakeHohders(address add)
+    //     public
+    //     returns (
+    //         uint256 totalStaked,
+    //         uint256 totalLockedBalance,
+    //         uint256 freeToUnstakeBalance,
+    //         uint256 lastBalancesUpdate,
+    //         uint256 totalReward,
+    //         uint256 lastRewardUpdate
+    //     )
+    // {
+    //     return (stakingContract.stakeholders(add));
+    // }
+
+    function checkMembership(address _memberAddress)
+        public
+        returns (uint256 membership)
+    {
+        (
+            uint256 totalStaked,
+            uint256 totalLockedBalance,
+            uint256 freeToUnstakeBalance,
+            uint256 lastBalancesUpdate,
+            uint256 totalReward,
+            uint256 lastRewardUpdate
+        ) = stakingContract.stakeholders(_memberAddress);
+
+        //TODO return number not int
+        uint256 membership = 0;
+        //TODO need to add NONE as tier as well
+
+        // update member balances
+        stakingContract.updateStakeholderBalances(_memberAddress);
+        // verifies if address is eligible for membership
+        //TODO weird number
+        if (totalLockedBalance < rubyMinimum) {
+            return membership;
+        }
+
+        for (
+            uint i = 0;
+            i < stakingContract.getStakingsLength(_memberAddress);
+            i++
+        ) {
+            (
+                uint256 totalStaked,
+                uint256 timestamp,
+                int256 stakingAmount,
+                uint256 lockDuration
+            ) = stakingContract.getStakingByIndex(_memberAddress, i);
+
+            if (
+                // check if staking is locked
+                lockDuration >= 90 days &&
+                block.timestamp - lockDuration < timestamp
+            ) {
+                // check if enough FTG staked for earning membership
+                if (stakingAmount < rubyMinimum) {
+                    //no privileges membership
+                    membership = 0;
+                } else if (
+                    stakingAmount >= rubyMinimum && stakingAmount < sapphireMinimum
+                ) {
+                    //ruby membership
+                    allAllocated[Tiers.RUBY] =
+                        allAllocated[Tiers.RUBY] -
+                        eachRubyTicket;
+                    ticketAllocated[Tiers.RUBY] =
+                        ticketAllocated[Tiers.RUBY] +
+                        eachRubyTicket;
+                    membership = membership > 1 ? membership : 1;
+                } else if (
+                    stakingAmount >= sapphireMinimum && stakingAmount < emeraldMinimum
+                ) {
+                    allAllocated[Tiers.SAPPHIRE] =
+                        allAllocated[Tiers.SAPPHIRE] -
+                        eachSapphireTicket;
+                    ticketAllocated[Tiers.SAPPHIRE] =
+                        ticketAllocated[Tiers.SAPPHIRE] +
+                        eachSapphireTicket;
+                    //sapphire membership
+                    membership = membership > 2 ? membership : 2;
+                } else if (
+                    stakingAmount >= emeraldMinimum && stakingAmount < diamondMinimum
+                ) {
+                    allAllocated[Tiers.EMERALD] =
+                        allAllocated[Tiers.EMERALD] -
+                        eachEmeraldTicket;
+                    ticketAllocated[Tiers.EMERALD] =
+                        ticketAllocated[Tiers.EMERALD] +
+                        eachEmeraldTicket;
+                    //emerald membership
+                    membership = 3;
+                } else {
+                    //diamond membership
+                    allAllocated[Tiers.RUBY] =
+                        allAllocated[Tiers.RUBY] -
+                        eachDiamondTicket;
+                    ticketAllocated[Tiers.RUBY] =
+                        ticketAllocated[Tiers.RUBY] +
+                        eachDiamondTicket;
+
+                    membership = 4;
+                    break;
+                }
+            }
+        }
+
+        return membership;
+    }
 }
